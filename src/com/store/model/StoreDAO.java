de14c96dc5f6232a9110c546f9a9d24d7b1701ec
joey155343 <joey155343@hotmaail.com>
2020-05-16 15:30:39 +0800

    ver2

--- src/com/store/model/StoreDAO.java
+++ src/com/store/model/StoreDAO.java
@@ -2,14 +2,24 @@ package com.store.model;
 
 import java.util.List;
 
-public class StoreDAO implements StoreDAO_interface {
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
+public class StoreDAO implements StoreDAO_interface {
+	private static DataSource ds = null;
 	public StoreDAO() {
 	}
 
 	@Override
 	public void insert(StoreVO storeVO) {
-
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/TestDB");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
