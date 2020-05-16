a7c2f9219dc53d519adf01862bdbb999d71e0a9e
joey155343 <joey155343@hotmaail.com>
2020-05-16 15:37:17 +0800

    master ver3

--- src/com/store/model/StoreDAO.java
+++ src/com/store/model/StoreDAO.java
@@ -12,6 +12,16 @@ public class StoreDAO implements StoreDAO_interface {
 	public StoreDAO() {
 	}
 
+	private static final String INSERT_STMT = 
+			"INSERT INTO emp1 (empno,ename,job,hiredate,sal,comm,deptno) VALUES (emp2_seq.NEXTVAL, ?, ?, ?, ?, ?, ?)";
+		private static final String GET_ALL_STMT = 
+			"SELECT empno,ename,job,to_char(hiredate,'yyyy-mm-dd') hiredate,sal,comm,deptno FROM emp2 order by empno";
+		private static final String GET_ONE_STMT = 
+			"SELECT empno,ename,job,to_char(hiredate,'yyyy-mm-dd') hiredate,sal,comm,deptno FROM emp2 where empno = ?";
+		private static final String DELETE = 
+			"DELETE FROM emp1 where empno = ?";
+		private static final String UPDATE = 
+			"UPDATE emp1 set ename=?, job=?, hiredate=?, sal=?, comm=?, deptno=? where empno = ?";
 	@Override
 	public void insert(StoreVO storeVO) {
 		try {
